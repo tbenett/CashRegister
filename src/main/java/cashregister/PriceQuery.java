2956b4010b79c2d5bf0c8cac610c569b4ca4a7d5
tho <tom@benett.io>
2018-05-18 10:07:46 +0200

    refacto(price-query): using reducer to find the item price from item code

--- src/main/java/cashregister/PriceQuery.java
+++ src/main/java/cashregister/PriceQuery.java
@@ -2,6 +2,8 @@ package cashregister;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.BiFunction;
 
 class PriceQuery {
 
@@ -12,13 +14,24 @@ class PriceQuery {
     }
 
     Result findPrice(String soughtItemCode) {
+        return reduce(
+            Result.notFound(soughtItemCode),
+            (result, itemReference) -> {
+                if (itemReference.matchesSoughtItemCode(soughtItemCode)) {
+                    return Result.found(itemReference.getUnitPrice());
+                } else {
+                    return result;
+                }
+            }, itemReferences);
+    }
 
-        for (ItemReference itemReference : itemReferences) {
-            if (itemReference.matchesSoughtItemCode(soughtItemCode)) {
-                return Result.found(itemReference.getUnitPrice());
-            }
+    <R, T> R reduce(R identity, BiFunction<R, T, R> reducer, Iterable<T> items) {
+        R accumulator = identity;
+
+        for (T item : items) {
+            accumulator = reducer.apply(accumulator, item);
         }
 
-        return Result.notFound(soughtItemCode);
+        return accumulator;
     }
 }
